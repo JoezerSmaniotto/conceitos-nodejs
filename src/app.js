1ded0cad015da903eb2fd8eed648fba520fd6aba
JoezerSmaniotto <jozersmaniotto5@gmail.com>
2020-05-13 15:42:27 -0300

    Atualização

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const cors = require("cors");
+const cors = require('cors');
 
 const { uuid } = require("uuidv4");
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
-app.use(cors());
+
 
 const repositories = [];
 
